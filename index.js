a80352dfe67e282895951c9d67fbd84a5eb71ef3
Hani <md.hani@hotmail.com>
2022-06-10 15:29:39 +0300

    commit tree 449767ada539b9c5235902065bad2f1a319e94a0

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  static sanitize(str) {
+    return str.replace( /[^A-Za-z0-9 '-]/g, '' );
+  }
+  static titleize(str) {
+    let excep= ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let arr = str.split(' ');
+    for (let i = 0; i < arr.length; i++) {
+      if (i===0){
+        arr[i] = this.capitalize(arr[i]);
+      }else if (excep.includes(arr[i])) {
+        arr[i] = arr[i].toLowerCase();
+      }else{
+        arr[i] = this.capitalize(arr[i]);
+      }
+    }
+    return arr.join(' ');
+  }
 }
+console.log (Formatter.titleize("a tale of two cities"));
